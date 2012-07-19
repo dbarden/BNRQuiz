f60a1ea7f34f7d419cfb5df3bce5cef82dd7d7c8
Daniel Barden <daniel@azapp.net>
2012-07-19 06:10:53 -0300

    Initial Xcode project commit

---
+++ BNRQuiz/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BNRQuiz
+//
+//  Created by Daniel Barden on 7/19/12.
+//  Copyright (c) 2012 Daniel Barden. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
